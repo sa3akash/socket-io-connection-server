d8ea9072ef4052ba96da27670766f16841cc4ccb
Shakil Ahmed <92939397+sa3akash@users.noreply.github.com>
2022-11-02 01:30:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//////////////// ES6
 // socket io connection
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -22,3 +24,35 @@ const io = new Server(httpServer, {
 httpServer.listen(process.env.PORT, ()=>{
     console.log(`server listening on ${process.env.PORT}`);
 })
+
+
+
+
+
+
+
+//// OLD
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server,{
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
+
+
+  // socket io works
+  io.on("connection", (socket) => {
+    console.log("Connected to socket...", socket.id);
+    // ...
+
+  });
+
+
+
+
+server.listen(PORT, ()=> console.log(`Listen on port ${PORT}`));
